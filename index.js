7549e410cc41f3f2cef40be533652a03e3e0ffe7
Yizuz9999 <paco123xs@gmail.com>
2025-01-20 16:23:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching Cold.cc Tickets"];
+const statusMessages = ["Watching Hexus Tickets"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
